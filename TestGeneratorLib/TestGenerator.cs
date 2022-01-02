71d5de639a49d9291ba409759121cc5a9affecf8
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-02 17:34:50 +0300

    init

---
+++ TestGeneratorLib/TestGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TestGeneratorLib
+{
+    class TestGenerator
+    {
+    }
+}

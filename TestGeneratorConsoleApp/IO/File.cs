7c1973b6480ba786f5ce2421cb69a21cee9973fe
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-03 14:28:50 +0300

    create file data type to be used in pipeline

---
+++ TestGeneratorConsoleApp/IO/File.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace TestGeneratorConsoleApp.IO
+{
+    internal class File
+    {
+        internal string FileName { get; }
+        internal string FolderPath { get; }
+        internal string FileContent { get; set; }
+
+        internal File(string folderPath, string filePath)
+        {
+            FolderPath = folderPath;
+            FileName = Path.GetFileNameWithoutExtension(filePath) + "Test" + "." + Path.GetExtension(filePath);
+        }
+
+        internal async Task ReadFromFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                FileContent = await reader.ReadToEndAsync();
+            }
+        }
+
+        internal async Task Write()
+        {
+            using (StreamWriter writer = new StreamWriter(FolderPath + "/" + FileName))
+            {
+                await writer.WriteAsync(FileContent);
+            }
+        }
+    }
+}

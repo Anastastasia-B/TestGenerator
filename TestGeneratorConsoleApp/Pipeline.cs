94ff421ccf971c462bf8be375eef57bc2f8e3f9a
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-03 14:27:29 +0300

    add pipeline

---
+++ TestGeneratorConsoleApp/Pipeline.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using TestGeneratorLib;
+using TestGeneratorConsoleApp.IO;
+using System.Threading;
+
+namespace TestGeneratorConsoleApp
+{
+    public class Pipeline
+    {
+        private string FileFolder { get; }
+
+        public Pipeline(string fileFolder)
+        {
+            FileFolder = fileFolder;
+        }
+
+        public async Task Processing(List<string> filesPath)
+        {
+            var readingBlock = new TransformBlock<string, File>(
+                async filePath => await ReadFile(filePath));
+            var generatingBlock = new TransformBlock<File, File>(
+                async file => await GenerateTest(file));
+            var writingBlock = new ActionBlock<File>(
+                async file => await WriteFile(file));
+
+            readingBlock.LinkTo(generatingBlock, new DataflowLinkOptions { PropagateCompletion = true });
+            generatingBlock.LinkTo(writingBlock, new DataflowLinkOptions { PropagateCompletion = true });
+
+            for (int i = 0; i < filesPath.Count; i++)
+            {
+                readingBlock.Post(filesPath[i]);
+            }
+            readingBlock.Complete();
+
+            await writingBlock.Completion;
+        }
+
+        private async Task<File> ReadFile(string filePath)
+        {
+            File file = new File(FileFolder, filePath);
+            await Task.Run(() => Thread.Sleep(8000));
+            return file;
+        }
+
+        private async Task<File> GenerateTest(File file)
+        {
+            await Task.Run(() => Thread.Sleep(8000));
+            return file;
+        }
+
+        private async Task WriteFile(File file)
+        {
+            await Task.Run(() => Thread.Sleep(8000));
+        }
+    }
+}
+
---
+++ TestGeneratorConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestGeneratorConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Pipeline pipeline = new Pipeline("C:\\Users\\anast\\source\\repos\\TestGenerator\\Results");
+            List<string> files = new List<string>() { "C:\\Users\\anast\\source\\repos\\TestGenerator\\Examples\\TreeClass.cs" };
+            await pipeline.Processing(files);
+        }
+    }
+}

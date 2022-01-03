0ad09848cb91155c2f538d376ece47df2e1dce97
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-03 14:31:40 +0300

    specify max degree of parallelism

--- TestGeneratorConsoleApp/Pipeline.cs
+++ TestGeneratorConsoleApp/Pipeline.cs
@@ -10,20 +10,42 @@ namespace TestGeneratorConsoleApp
     public class Pipeline
     {
         private string FileFolder { get; }
+        private int FilesInputCount { get; set; } = 1;
+        private int FilesOutputCount { get; set; } = 1;
+        private int TasksCount { get; set; } = 1;
 
         public Pipeline(string fileFolder)
         {
             FileFolder = fileFolder;
         }
 
+        public void SetParameters(int filesInputCount, int filesOutputCount, int tasksCount)
+        {
+            FilesInputCount = filesInputCount;
+            FilesOutputCount = filesOutputCount;
+            TasksCount = tasksCount;
+        }
+
         public async Task Processing(List<string> filesPath)
         {
             var readingBlock = new TransformBlock<string, File>(
-                async filePath => await ReadFile(filePath));
+                async filePath => await ReadFile(filePath),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = FilesInputCount
+                });
             var generatingBlock = new TransformBlock<File, File>(
-                async file => await GenerateTest(file));
+                async file => await GenerateTest(file),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = TasksCount
+                });
             var writingBlock = new ActionBlock<File>(
-                async file => await WriteFile(file));
+                async file => await WriteFile(file),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = FilesOutputCount
+                });
 
             readingBlock.LinkTo(generatingBlock, new DataflowLinkOptions { PropagateCompletion = true });
             generatingBlock.LinkTo(writingBlock, new DataflowLinkOptions { PropagateCompletion = true });
--- TestGeneratorConsoleApp/Program.cs
+++ TestGeneratorConsoleApp/Program.cs
@@ -9,6 +9,7 @@ namespace TestGeneratorConsoleApp
         static async Task Main(string[] args)
         {
             Pipeline pipeline = new Pipeline("C:\\Users\\anast\\source\\repos\\TestGenerator\\Results");
+            pipeline.SetParameters(2, 2, 3);
             List<string> files = new List<string>() { "C:\\Users\\anast\\source\\repos\\TestGenerator\\Examples\\TreeClass.cs" };
             await pipeline.Processing(files);
         }
